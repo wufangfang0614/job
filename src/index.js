7e3b4f94aa847223be3b0275ed46aeedd942218e
wufangfang0614 <wufangfang@neotel.com.cn>
2019-01-07 23:37:36 +0800

    bossinfo

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import { BrowserRouter, Route, Redirect,Switch } from 'react-router-dom'
 import AuthRoute from './component/authroute/authroute'
 import Login from './container/login/login'
 import Register from './container/register/register'
+import BossInfo from './container/bossinfo/bossinfo'
 import reducers from './reducer'
 import './config'
 import './index.css'
@@ -16,16 +17,14 @@ const store = createStore(reducers, compose(
 	applyMiddleware(thunk),
 	window.devToolsExtension?window.devToolsExtension():f=>f
 ))
-function Boss(){
-	return <h2>BOSS页面</h2>
-}
+ 
 ReactDom.render(
 	(<Provider store={store}>
 		<BrowserRouter>
 			<div>
 				
 				<AuthRoute></AuthRoute>
-				<Route path='/boss' component={Boss}></Route>
+				<Route path='/bossinfo' component={BossInfo}></Route>
 				<Route path='/login' component={Login}></Route>
 				<Route path='/register' component={Register}></Route>
 			</div>
